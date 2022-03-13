d7374a54ab3ad4d836710daa48123ac3aa611b1c
YUAN-MAX <747285133@qq.com>
2022-03-14 00:16:21 +0300

    Files for Lab1

---
+++ ClassLibrary1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using System.Windows.Data;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataLibrary;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    class MyTextshow : INotifyPropertyChanged
+    {
+        public string show;//显示
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Show
+        {
+            get { return show; }
+            set
+            {
+                show = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Show"));
+            }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        ViewData viewData = new ViewData();
+        VMf vMf = new VMf();
+        VMGird vMGird = new VMGird();
+        VMBenchmark vMBenchmark = new VMBenchmark();
+        public MainWindow()
+        {
+            InitializeComponent();
+            grid_sample.DataContext = viewData.vMBenchmark.vMtimes;
+            grid_VMA.DataContext = viewData.vMBenchmark.vMAccuracies;
+        }
+
+
+        private bool UnsavedChanges()
+        {
+            MessageBoxResult mes = MessageBox.Show("Do you want to save the data as a file?", "Save", MessageBoxButton.YesNoCancel);
+            if (mes == MessageBoxResult.Yes)
+            {
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.FileName = "filename";
+                dlg.DefaultExt = ".txt";
+                dlg.Filter = "TXTFiles(.txt)|*.txt";
+                if ((bool)dlg.ShowDialog())
+                    viewData.Save(dlg.FileName);
+            }
+            else if (mes == MessageBoxResult.Cancel)
+                return true;
+            return false;
+        }
+        private void New_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (viewData.CollectionChangedAfterSave)
+                UnsavedChanges();
+            viewData = new ViewData();
+            grid_sample.DataContext = viewData.vMBenchmark.vMtimes;
+            grid_VMA.DataContext = viewData.vMBenchmark.vMAccuracies;
+            tname.Text = "";
+            textBox.Text = "";
+            MessageError();
+        }
+        private void Open_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (viewData.CollectionChangedAfterSave)
+                UnsavedChanges();
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            if ((bool)dlg.ShowDialog())
+            {
+                viewData = new ViewData();
+                viewData.Load(dlg.FileName);
+                grid_sample.DataContext = viewData.vMBenchmark.vMtimes;
+                grid_VMA.DataContext = viewData.vMBenchmark.vMAccuracies;
+            }
+            MessageError();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Inf";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "TXTFiles(.txt)|*.txt";
+            if ((bool)dlg.ShowDialog())
+                viewData.Save(dlg.FileName);
+            MessageError();
+        }
+        private void AddVMtime_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] words = tname.Text.Split(';');
+                vMGird.Left_endpoint = float.Parse(words[0]);
+                vMGird.Right_endpoint = float.Parse(words[1]);
+                vMGird.Length_vector = int.Parse(textBox.Text);
+                if (ComboBox1.Text == "VMSLN and VMDLN")
+                {
+                    vMf = VMf.VMSLN_VMDLN;
+                }
+                else if (ComboBox1.Text == "VMSLGamma and VMDLGamma")
+                {
+                    vMf = VMf.VMSLGamma_VMDLGamma;
+                }
+                viewData.AddVMtime(vMf, vMGird);
+                information_block.Text = DateTime.Now + "\n" + "Successfully added a new element VMtime";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Формат ввода не верный");
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void AddVMAccuracy_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] words = tname.Text.Split(';');
+                vMGird.Left_endpoint = float.Parse(words[0]);
+                vMGird.Right_endpoint = float.Parse(words[1]);
+                vMGird.Length_vector = int.Parse(textBox.Text);
+                if (ComboBox1.Text == "VMSLN and VMDLN")
+                {
+                    vMf = VMf.VMSLN_VMDLN;
+                }
+                else if (ComboBox1.Text == "VMSLGamma and VMDLGamma")
+                {
+                    vMf = VMf.VMSLGamma_VMDLGamma;
+                }
+                viewData.AddVMAccuracy(vMf, vMGird);
+                information_block.Text = DateTime.Now + "\n" + "Successfully added a new element VMAccuracy";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Формат ввода не верный");
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs cl)
+        {
+            if (viewData.CollectionChangedAfterSave)
+                UnsavedChanges();
+            MessageError();
+        }
+
+
+        public void MessageError()
+        {
+            if (viewData.ErrorMessage != null)
+            {
+                MessageBox.Show(viewData.ErrorMessage, "Error");
+                viewData.ErrorMessage = null;
+            }
+        }
+
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (ListBox1.SelectedItem != null)
+                    Block1.Text = ListBox1.SelectedItem.ToString();
+                if (ListBox2.SelectedItem != null)
+                {
+                    Max_error_block.Text = ListBox2.SelectedItem.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Element is empty");
+            }
+        }
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+
+        }
+        private void TextBox_Leave(object sender, System.EventArgs e)
+        {
+            // Reset the colors and selection of the TextBox after focus is lost.
+
+        }
+        private void tname_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+        }
+
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ClassLibrary1/WpfApp1/ViewData.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLibrary;
+
+
+using System.Collections;
+
+using System.ComponentModel;
+using System.Numerics;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.CompilerServices;
+using System.Collections.Specialized;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace WpfApp1
+{
+    class ViewData
+    {
+        public VMBenchmark vMBenchmark = new VMBenchmark();
+        [field: NonSerialized]
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnCollectionChanged(NotifyCollectionChangedAction ev)
+        {
+            if (CollectionChanged != null)
+                CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public bool CollectionChangedAfterSave { get; set; }
+
+        public string ErrorMessage
+        {
+            get; set;
+        }
+
+        public void AddVMtime(VMf vMf, VMGird vMGird)
+        {
+            try
+            {
+
+
+                vMBenchmark.AddVMtime(vMf, vMGird);
+                OnCollectionChanged(NotifyCollectionChangedAction.Add);
+                /*OnPropertyChanged("");*/
+                CollectionChangedAfterSave = true;
+                OnPropertyChanged("CollectionChangedAfterSave");
+            }
+            catch (Exception ex)
+            {
+                this.ErrorMessage = "Add failed: " + ex.Message;
+            }
+        }
+        public void AddVMAccuracy(VMf vMf, VMGird vMGird)
+        {
+            try
+            {
+
+                vMBenchmark.AddVMAccuracy(vMf, vMGird);
+                OnCollectionChanged(NotifyCollectionChangedAction.Add);
+                /*OnPropertyChanged("");*/
+                CollectionChangedAfterSave = true;
+                OnPropertyChanged("CollectionChangedAfterSave");
+            }
+            catch (Exception ex)
+            {
+                this.ErrorMessage = "Add failed: " + ex.Message;
+            }
+        }
+        public void Save(string filename)
+        {
+            Directory.SetCurrentDirectory("..\\..\\..\\");
+            FileStream FS = null;
+
+            try
+            {
+                if (File.Exists(filename))
+                    FS = File.OpenWrite(filename);
+                else
+                    FS = File.Create(filename);
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(FS, vMBenchmark);
+            }
+            catch (Exception ex)
+            {
+                this.ErrorMessage = "Save failed:" + ex.Message;
+            }
+            finally
+            {
+                if (FS != null)
+                    FS.Close();
+                CollectionChangedAfterSave = false;
+                OnPropertyChanged("CollectionChangedAfterSave");
+            }
+        }
+
+        public bool Load(string filename)
+        {
+            Directory.SetCurrentDirectory("..\\..\\..\\");
+            FileStream FS = null;
+
+            try
+            {
+                FS = File.OpenRead(filename);
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                vMBenchmark = (VMBenchmark)binaryFormatter.Deserialize(FS);
+            }
+            catch (Exception ex)
+            {
+                this.ErrorMessage = "Load: " + ex.Message;
+            }
+            finally
+            {
+                FS.Close();
+                CollectionChangedAfterSave = true;
+                OnPropertyChanged("CollectionChangedAfterSave");
+            }
+
+            return true;
+        }
+
+    }
+}
